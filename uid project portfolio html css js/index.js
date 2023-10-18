4f3e330e0f87a5b35dad08a4b793c48e7c257c96
ashishk123455 <117940223+ashishk123455@users.noreply.github.com>
2023-10-18 22:19:17 +0530

    Add files via upload

---
+++ uid project portfolio html css js/index.js
@@ -0,0 +1,25 @@
+let nav=document.querySelector('nav');
+
+        window.addEventListener('scroll',()=>{
+            nav.classList.toggle('nav-scroll',window.scrollY>0);
+        });
+        //Active Toggle
+        let AllNavLinkA=document.querySelectorAll('.nav-links li a')
+
+        //Active Add.....
+        AllNavLinkA.forEach(item=>{
+            item.addEventListener('click',()=>{
+                removeActive()
+                item.classList.add('active');
+            })
+        })
+        //Active Remove.....
+        const removeActive=()=>AllNavLinkA.forEach(item=>{
+            item.classList.remove('active')
+        })
+        //Nav Links Toggler...
+        let menuBars=document.querySelector('nav button');
+        let navLinks=document.querySelector('.nav-links')
+        menuBars.addEventListener('click',()=>{
+            navLinks.classList.toggle('nav-links')
+        })
